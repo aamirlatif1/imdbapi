d7f07728157ffc7d8c1b05de55b8fbc96598de9d
aamirlatif1 <aamirlatif@gmail.com>
2026-09-12 20:30:05 +0200

    add validations for create movie

--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -43,8 +44,12 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data any, h
 	return nil
 }
 
-func (app *application) readJSON(r *http.Request, dest interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(dest)
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dest any) error {
+	r.Body = http.MaxBytesReader(w, r.Body, 1_048_576)
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dest)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -57,7 +62,7 @@ func (app *application) readJSON(r *http.Request, dest interface{}) error {
 		case errors.Is(err, io.ErrUnexpectedEOF):
 			return errors.New("body contains badly-formed JSON")
 
-		case errors.As(err, &invalidUnmarshalFieldError):
+		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != "" {
 				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
 			}
@@ -66,12 +71,22 @@ func (app *application) readJSON(r *http.Request, dest interface{}) error {
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
 
-		case errors.As(err, &unmarshalTypeError):
+		case errors.As(err, &invalidUnmarshalFieldError):
 			panic(err)
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown field %q", fieldName)
 
 		default:
 			return err
 		}
 	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
 	return nil
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/aamirlatif1/imdbapi/internal/data"
+	"github.com/aamirlatif1/imdbapi/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,12 +18,29 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	var in input
-	err := app.readJSON(r, &in)
+	err := app.readJSON(w, r, &in)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
 
+	v := validator.New()
+	v.Check(in.Title != "", "title", "required")
+	v.Check(len(in.Title) <= 500, "title", "must not be larger than 500 bytes")
+
+	v.Check(in.Runtime > 0, "runtime", "must be larger than 0")
+
+	v.Check(in.Year != 0, "year", "must be larger than 0")
+	v.Check(in.Year > 1888, "year", "must not be larger than 1888 bytes")
+
+	v.Check(in.Genres != nil, "runtime", "must be provided")
+	v.Check(len(in.Genres) > 0, "genres", "must not be empty")
+	v.Check(len(in.Genres) <= 5, "genres", "must not be more than 5")
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
 	fmt.Fprintf(w, "%+v\n", in)
 }
 
